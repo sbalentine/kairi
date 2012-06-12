5726b9924f71e541b7b488715cefa44cb0521498
Scott Balentine <sbalentine@yammer-inc.com>
2012-06-11 18:21:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kairi::Application.config.secret_token = 'd5b286b76fa510a447f06a1801e1294fa847a4935a3ff87d50123112b89f6212264f4c5046aa31895bb1de05d9df36eab929675a79b661e1408e5e00e3f3d7dd'
