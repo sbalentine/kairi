9459c1e93d6fe0331f643990dbe7b71962458c65
Scott Balentine <sbalentine@yammer-inc.com>
2012-07-08 21:54:23 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,44 +7,28 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
     
-    it { should have_selector('h1', :text =>'Home') }
-    it { should have_selector('title', :text => "Kairi") }
-    it { should_not have_selector('title', :text => '| Home') }
+    it { should have_selector('h1',       text: 'Home') }
+    it { should have_selector('title',    text: full_title('')) }
   end
   
   describe "Help page" do
     before { visit help_path }
     
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', :text =>'Help')
-    end
-    
-    it "should have the title 'Help'" do
-      page.should have_selector('title', :text => "Kairi | Help")
-    end
+    it { should have_selector('h1',       text: 'Help') }
+    it { should have_selector('title',    text: full_title('')) }
   end
   
   describe "About page" do
     before { visit about_path }
     
-    it "should have the h1 'About Us'" do
-      page.should have_selector('h1', :text =>'About Us')
-    end
-    
-    it "should have the title 'About Us'" do
-      page.should have_selector('title', :text => "Kairi | About Us")
-    end
+    it { should have_selector('h1',       text: 'About') }
+    it { should have_selector('title',    text: full_title('')) }
   end
   
   describe "Contact page" do
     before { visit contact_path }
     
-    it "should have the h1 'Contact'" do
-      page.should have_selector('h1', :text => 'Contact')
-    end
-    
-    it "should have the title 'Contact'" do
-      page.should have_selector('title', :text => 'Kairi | Contact')
-    end
+    it { should have_selector('h1',       text: 'Contact') }
+    it { should have_selector('title',    text: full_title('')) }
   end
 end
